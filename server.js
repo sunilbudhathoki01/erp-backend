3b077e47abcb9e7b020ae5d2fe7195598a612ba5
Sunil Budhathoki <ashishbudhathoki952@gmail.com>
2026-06-28 15:35:46 +0000

    ci: add dockerfile sandbox and github actions workflow

---
+++ server.js
@@ -0,0 +1,4 @@
+console.log("--------------------------------------------------");
+console.log("ERP Sandbox running flawlessly inside Docker!");
+console.log("Current Environment: Linux (WSL Ubuntu)");
+console.log("--------------------------------------------------");
