e93598b7d0e6160995fe62b55bae0627c44f2b08
Sunil Budhathoki <ashishbudhathoki952@gmail.com>
2026-07-16 10:39:36 +0000

    M2:dockerize postress +  typeorm connection via configodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dbConfig from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal:true,load:[dbConfig]}),TypeOrmModule.forRootAsync
+    ({imports:[ConfigModule],inject:[ConfigService],
+ useFactory: (config: ConfigService) => ({
+  type: 'postgres',
+  host: config.get<string>('database.host'),
+  port: config.get<number>('database.port'),
+  username: config.get<string>('database.username'),
+  password: config.get<string>('database.password'),
+  database: config.get<string>('database.name'),
+  autoLoadEntities: true,
+  synchronize: config.get<boolean>('database.synchronize'),
+})})],
   controllers: [AppController],
   providers: [AppService],
 })
