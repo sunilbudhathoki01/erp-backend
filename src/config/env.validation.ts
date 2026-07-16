d5f4fd07dd3be3c176e668f17b110201b20e7a86
Sunil Budhathoki <ashishbudhathoki952@gmail.com>
2026-07-16 11:01:02 +0000

    M3:add env validation with joi

---
+++ src/config/env.validation.ts
@@ -0,0 +1,15 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_SYNCHRONIZE: Joi.boolean().default(false),
+});
